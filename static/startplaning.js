97dfae6897de843c6ae43ebeac7d29dee52ee6eb
Roza Alene <rozaalene@gmail.com>
2023-10-15 04:40:19 +0300

    fetch json data from the files

--- static/startplaning.js
+++ static/startplaning.js
@@ -1,29 +1,48 @@
 var welcomeMessage = document.getElementById("welcomeid").textContent;
-var wel_id = welcomeMessage.split(" ")[1];
-
+var wel_id = parseInt(welcomeMessage.split(" ")[1]);
+const ul = document.getElementById('expense');
+const ulincome = document.getElementById('income')
 console.log(wel_id);
 
-fetch('/get_json')
+let expense_sum = 0;
+let income_sum = 0;
+fetch('/get_incomejson')
   .then(response => response.json())
-  .then(data => {
-    const jsonData = JSON.parse(data);
-    // Process the JSON data
-    console.log(jsonData);
-
-    // Filter expenses based on user ID
-    const userExpenses = jsonData.filter(expense => expense.user_id === wel_id);
-
-    // Process the user's expenses
-    console.log(userExpenses);
-
-    // Print the amount and category on the page
-    const expensesContainer = document.getElementById("expensesContainer");
-    userExpenses.forEach(expense => {
-      const expenseElement = document.createElement("p");
-      expenseElement.textContent = `Category: ${expense.category}, Amount: ${expense.amount}`;
-      expensesContainer.appendChild(expenseElement);
-    });
+  .then(income_data => {
+    const filterincome = income_data.income.filter(income => income.user_id === wel_id);
+    filterincome.forEach(income => {
+      const li = document.createElement('li');
+      li.textContent = `${income.category} = ${income.amount}`;
+      income_sum += income.amount;
+      ulincome.appendChild(li);
+    })
+    const incomepar = document.createElement('p');
+    incomepar.innerHTML = "Total income" + income_sum;
+    document.body.appendChild(incomepar);
   })
   .catch(error => {
     console.error('Error:', error);
   });
+fetch('/get_expensejson')
+  .then(response => response.json())
+  .then(expense_data => {
+    const filteredExpenses = expense_data.expense.filter(expense => expense.user_id === wel_id);
+    filteredExpenses.forEach(expense => {
+      const li = document.createElement('li');
+      li.textContent = `${expense.category} = ${expense.amount}`;
+      expense_sum += expense.amount;
+      ul.appendChild(li);
+    });
+  const paragraph = document.createElement('p');
+  paragraph.innerHTML = "Total sum of your planned spending" + expense_sum;
+  document.body.appendChild(paragraph);
+
+  const difference = income_sum - expense_sum;
+  const differencepara = document.createElement('p');
+  differencepara.innerHTML = "you remain amount is " + difference;
+  document.body.appendChild(differencepara);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
