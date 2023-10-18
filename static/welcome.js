a255c5261cd2300051889b8f9e44f9c557a439f0
Roza Alene <rozaalene@gmail.com>
2023-10-18 05:51:50 +0300

    calculation for expense and spend

--- static/welcome.js
+++ static/welcome.js
@@ -1,12 +1,18 @@
 var welcomeMessage = document.getElementById("welcomeid").textContent;
 var wel_id = parseInt(welcomeMessage.split(" ")[1]);
 const totalBudgetDiv = document.getElementById('total_budget');
+const totalspending = document.getElementById('remaining_budget')
 const budget_chart = document.getElementById("budget_chart");
 const graph_budget = document.getElementById('graph');
 const total = document.getElementById('budget');
+const totalincome = document.getElementById('income');
 const list_category = document.getElementById('dropdown');
+const topthree = document.getElementById('highest');
+const expanse_list = document.getElementById("expense")
 
 let expense_sum = 0;
+let spend_sum = 0;
+let income_sum = 0;
 let category_array = [];
 let amount_array = [];
 
@@ -19,6 +25,7 @@ function randomColor() {
   // Return the random color in RGB format
   return `rgb(${r}, ${g}, ${b})`;
 }
+
 
 function category_list(data) {
   data.forEach(list => {
@@ -73,9 +80,108 @@ fetch('/get_expensejson')
       value: category,
       text: category
     }));
-
     category_list(dropdownData);
-  })
+
+    fetch('/get_spendjson')
+      .then(response => response.json())
+      .then(spend_data => {
+        const filterspend = spend_data.spending.filter(spend => spend.user_id === wel_id);
+
+        // Create an object to store the similar spendings
+        const similarspendings = {};
+
+        filterspend.forEach(spend => {
+          const category = spend.category;
+          const amount = spend.amount;
+
+          if (similarspendings.hasOwnProperty(category)) {
+            // If the category already exists in the object, add the amount to the existing value
+            similarspendings[category] += amount;
+          } else {
+            // If the category doesn't exist, create a new entry with the amount
+            similarspendings[category] = amount;
+          }
+        });
+
+        // Sort the similar spendings in descending order based on the total amount
+        const sortedSpendings = Object.entries(similarspendings)
+          .sort((a, b) => b[1] - a[1]);
+
+        // Get the top three spendings
+        const topThreeSpendings = sortedSpendings.slice(0, 3);
+
+        topThreeSpendings.forEach(([category, amount]) => {
+          spend_sum += amount;
+          console.log(expense.amount)
+          const spendItem = document.createElement('li');
+          spendItem.innerHTML = `${category} = ${amount}`;
+          topthree.appendChild(spendItem);
+        });
+
+        const paragraph2 = document.createElement('p');
+        paragraph2.innerHTML = expense_sum - spend_sum;
+        totalspending.appendChild(paragraph2);
+      })
       .catch(error => {
         console.error('Error:', error);
       });
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+fetch('/get_incomejson')
+  .then(response => response.json())
+  .then(income_data => {
+    const filterincome = income_data.income.filter(income => income.user_id === wel_id);
+    filterincome.forEach(income => {
+      income_sum += income.amount;
+    });
+
+    const incomepar = document.createElement('p');
+    incomepar.innerHTML = income_sum;
+    totalincome.appendChild(incomepar);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+
+  fetch('/get_expensejson')
+  .then(response => response.json())
+  .then(expense_data => {
+    const filteredExpenses = expense_data.expense.filter(expense => expense.user_id === wel_id);
+
+    // Get spending data
+    fetch('/get_spendjson')
+      .then(response => response.json())
+      .then(spend_data => {
+        const filteredSpending = spend_data.spending.filter(spend => spend.user_id === wel_id);
+
+        // Iterate over expense data
+        // Iterate over expense data
+        filteredExpenses.forEach(expense => {
+          const category = expense.category;
+          const expenseAmount = expense.amount;
+          const li = document.createElement('li');
+          const spendingAmount = filteredSpending
+            .filter(spend => spend.category === category)
+            .reduce((total, spend) => total + spend.amount, 0);
+
+          const remainingAmount = expenseAmount - spendingAmount;
+
+          li.textContent = `${category} = ${expenseAmount} Spend: ${spendingAmount}, Remaining: ${remainingAmount}`;
+          expanse_list.appendChild(li);
+
+          // Check if spending exceeds expense plan
+          if (spendingAmount > expenseAmount) {
+            li.style.color = 'red'; // Change the color of the list item to red
+          }
+        });
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
