c18f54b819cc7132b3143c1914a8591b9777dd5d
Roza Alene <rozaalene@gmail.com>
2023-10-19 03:53:46 +0300

    edit and delete for income

--- static/startplaning.js
+++ static/startplaning.js
@@ -12,10 +12,66 @@ fetch('/get_incomejson')
   .then(income_data => {
     const filterincome = income_data.income.filter(income => income.user_id === wel_id);
     filterincome.forEach(income => {
+      const incomeContainer = document.createElement('div');
+      incomeContainer.setAttribute('class', 'income_container');
       const li = document.createElement('li');
+
       li.textContent = `${income.category} = ${income.amount}`;
       income_sum += income.amount;
-      ulincome.appendChild(li);
+      
+      const editbutton = document.createElement("button");
+      editbutton.textContent = "Edit";
+      editbutton.setAttribute('class', 'update');
+      editbutton.setAttribute('name', 'update');
+
+      
+
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = 'Delete';
+      deleteButton.setAttribute('class', 'delete');
+      deleteButton.setAttribute('type', 'submit');
+
+      const form = document.createElement('form');
+      form.setAttribute('method', 'POST');
+      form.setAttribute('action', '/delete_income');
+
+      form.appendChild(deleteButton);
+
+      deleteButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        const incomeItem = this.parentNode.parentNode; // Get the parent element of the form (incomeContainer)
+        const dataToDelete = incomeItem.querySelector('li').textContent; // Get the text content of the li element
+        const amountToDelete = parseFloat(dataToDelete.split('=')[1].trim());
+
+        console.log('Delete URL:', '/delete_income');
+        fetch('/delete_income', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ data: dataToDelete, user_id: wel_id })
+        })
+          .then(response => response.json())
+          .then(result => {
+            // Handle the response from the server if needed
+            console.log(result);
+            // Remove the deleted item from the DOM
+            incomeItem.remove();
+
+            income_sum -= amountToDelete;
+            incomepar.innerHTML = "Total income: " + income_sum;
+            
+          })
+          .catch(error => {
+            // Handle any errors that occurred during the request
+            console.error('Error:', error);
+          });
+      });
+
+      incomeContainer.appendChild(li);
+      incomeContainer.appendChild(editbutton);
+      incomeContainer.appendChild(form);
+      ulincome.appendChild(incomeContainer);
     });
 
     const incomepar = document.createElement('p');
@@ -26,25 +82,25 @@ fetch('/get_incomejson')
       .then(response => response.json())
       .then(expense_data => {
         const filteredExpenses = expense_data.expense.filter(expense => expense.user_id === wel_id);
-  
-        // Sort the expenses in descending order based on the amount
+
         filteredExpenses.sort((a, b) => b.amount - a.amount);
-  
+
         filteredExpenses.forEach(expense => {
           const li = document.createElement('li');
           li.textContent = `${expense.category} = ${expense.amount}`;
           expense_sum += expense.amount;
+          
           ul.appendChild(li);
         });
-  
+
         const paragraph = document.createElement('p');
-        paragraph.innerHTML = "Total sum of your planned spending: " + expense_sum;
+        paragraph.innerHTML = "Total sum of your planned Expense: " + expense_sum;
         document.body.appendChild(paragraph);
-  
-        // Calculate and display the remaining amount
+        console.log('Expense sum:', expense_sum);
+
         const difference = income_sum - expense_sum;
         const differencepara = document.createElement('p');
-        differencepara.innerHTML = "Your remaining amount is: " + difference;
+        differencepara.innerHTML = "Available balance " + difference;
         document.body.appendChild(differencepara);
       })
       .catch(error => {
